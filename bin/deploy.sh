ceacb7a7e5ca2ae6c19c04592e6e62772725de25
Geoff Waggott <geoff.waggott@digital.homeoffice.gov.uk>
2016-11-15 11:51:54 +0000

    added kube files

---
+++ bin/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export KUBE_NAMESPACE=${KUBE_NAMESPACE:-${DRONE_DEPLOY_TO}}
+export KUBE_SERVER=${KUBE_SERVER_DEV}
+export KUBE_TOKEN=${KUBE_TOKEN_DEV}
+
+cd kube
+kd --insecure-skip-tls-verify \
+   --file deployment.yaml \
+   --file service.yaml
